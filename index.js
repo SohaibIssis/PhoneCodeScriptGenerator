ea7244dcff128e5d80d3ea85331202b0a458fb8a
sohaib issis <sohaibissis@yahoo.com>
2022-05-18 21:31:18 +0100

    write result to file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const LibJs = require('libphonenumber-js');
+const fs = require('fs');
 var countries = LibJs.getCountries();
+let output = '';
 countries.forEach((country) => {
-   console.log(
-      `update country set "PhonePrefix" = ${LibJs.getPhoneCode(
-         country
-      )} where "CountryCode" = '${country}'`
-   );
+   output += `update country set "PhonePrefix" = '${LibJs.getPhoneCode(
+      country
+   )}' where "CountryCode" = '${country}'; \n`;
 });
+fs.writeFileSync('output.sql', output);
